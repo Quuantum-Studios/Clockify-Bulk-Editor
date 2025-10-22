38128c86036c090af668fbd963176107b2e62829
dheeraj-yadav29 <130667959+dheeraj-yadav29@users.noreply.github.com>
2025-10-23 02:19:40 +0530

    GA4 added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "ClockifyManager"
 
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || ''} />
     </html>
   );
 }
